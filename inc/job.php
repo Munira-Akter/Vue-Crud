57688958bffefcb71e63869e329f8d10717fb42f
Munira Akter <munira660602@gmail.com>
2021-05-26 20:30:53 +0600

    Add Search functionality

--- inc/job.php
+++ inc/job.php
@@ -34,3 +34,156 @@
      
     }
 
+    // Show all data from database
+
+    if($action == 'index'){
+
+        $data = $conn -> query("SELECT * FROM job ORDER BY id DESC");
+
+        $all_data = [];
+        while($single_data = $data->fetch_assoc()){
+            array_push($all_data ,  $single_data);
+        }
+
+        echo json_encode($all_data);
+
+    }
+
+     // Delete single data from database
+
+     if($action == 'delete'){
+       $id = '';
+       if(isset($_GET['id'])){
+          $id = $_GET['id'];
+       }
+
+       $conn -> query("DELETE FROM job WHERE id='$id'");
+
+    }
+
+    // Single data show
+
+    if($action == 'show'){
+
+        $id = '';
+        if(isset($_GET['id'])){
+           $id = $_GET['id'];
+        }
+        $data = $conn -> query("SELECT * FROM job WHERE id = '$id'");
+
+        $dataf = $data -> fetch_assoc();
+
+        echo $datas = json_encode($dataf);
+
+    }
+
+     // Single edit data show
+
+     if ($action == 'edit') {
+         $id = '';
+         if (isset($_GET['id'])) {
+             $id = $_GET['id'];
+         }
+
+
+         $data = $conn -> query("SELECT * FROM job WHERE id = '$id'");
+
+         $dataf = $data -> fetch_assoc();
+
+         echo $datas = json_encode($dataf);
+     }
+
+
+    // Show all data from database by txt search
+
+    if($action == 'searchtxt'){
+
+        $txt = $_GET['txt'];
+
+        $data = $conn -> query("SELECT * FROM job WHERE title LIKE '%$txt%' ");
+
+        $all_data = [];
+        while($single_data = $data->fetch_assoc()){
+            array_push($all_data ,  $single_data);
+        }
+
+        echo $datat = json_encode($all_data);
+
+    }
+
+    // Show all data from database by txt search
+
+    if($action == 'searchtxtBtn'){
+
+        $txt = $_GET['txt'];
+
+        $data = $conn -> query("SELECT * FROM job WHERE title LIKE '%$txt%' ");
+
+        $all_data = [];
+        while($single_data = $data->fetch_assoc()){
+            array_push($all_data ,  $single_data);
+        }
+
+        echo $datat = json_encode($all_data);
+
+    }
+
+    // Show all data from database by range bar
+
+    if($action == 'searchtxtrange'){
+
+        $txt = $_GET['txt'];
+
+        $data = $conn -> query("SELECT * FROM job WHERE budget BETWEEN  0 AND '$txt' ");
+
+        $all_data = [];
+        while($single_data = $data->fetch_assoc()){
+            array_push($all_data ,  $single_data);
+        }
+
+        echo $datat = json_encode($all_data);
+
+    }
+
+    // Show all data from database by range bar
+
+    if($action == 'searchRemote'){
+
+        $txt = $_GET['txt'];
+
+        $data = $conn -> query("SELECT * FROM job WHERE location LIKE '%$txt%'");
+
+        $all_data = [];
+        while($single_data = $data->fetch_assoc()){
+            array_push($all_data ,  $single_data);
+        }
+
+        echo $datat = json_encode($all_data);
+
+    }
+ 
+
+ 
+
+// Show all data from database by range bar
+
+if($action == 'searchWorld'){
+
+    $txt = $_GET['txt'];
+
+    $data = $conn -> query("SELECT * FROM job WHERE location LIKE '%$txt%'");
+
+    $all_data = [];
+    while($single_data = $data->fetch_assoc()){
+        array_push($all_data ,  $single_data);
+    }
+
+    echo $datat = json_encode($all_data);
+
+}
+
+
+
+
+
+
